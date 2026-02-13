fa55cab813ba744215415d03a784035a8ac68a04
rex-1234 <jitendrachoudhary128@gmail.com>
2026-02-13 22:52:35 +0530

    Added schema and post creation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
@@ -42,11 +43,23 @@ def post_page(request: Request, post_id: int):
             return templates.TemplateResponse("post.html", {"request": request, "post": post, "title": title})
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
     return posts
 
-@app.get("/api/posts/{post_id}")
+@app.post("/api/posts", response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+def create_post(post: PostCreate):
+    new_post = {
+        "id": len(posts) + 1,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "Feb 10, 2026",
+    }
+    posts.append(new_post)
+    return new_post
+
+@app.get("/api/posts/{post_id}", response_model=PostResponse)
 def get_post(post_id: int):
     for post in posts:
         if post["id"] == post_id:
